989df8b6e8de60c08902fa667cf15dbf56c1f582
Jason Scott <jason.scott5391@gmail.com>
2014-10-28 19:21:37 -0400

    Refactoring, commenting, and calculation of distances between occurences

--- Vigenere.c
+++ Vigenere.c
@@ -30,65 +30,113 @@ int main(int argc, char **argv)
 
   size_t cipherlen = strlen(ciphertext);
   
-  char * segmentptr = (char *)malloc(sizeof(char) * cipherlen + 1);
-  char * searchptr = (char *)malloc(sizeof(char) * cipherlen + 1);
+  char * trigraphptr = (char *)malloc(sizeof(char) * cipherlen + 1);
+  char * searchtriptr = (char *)malloc(sizeof(char) * cipherlen + 1);
 
-  strncpy(segmentptr, ciphertext, cipherlen);
-  strncpy(searchptr, ciphertext, cipherlen);
-  segmentptr[cipherlen] = '\0';
-  searchptr[cipherlen] = '\0';
+  strncpy(trigraphptr, ciphertext, cipherlen);
+  strncpy(searchtriptr, ciphertext, cipherlen);
+  trigraphptr[cipherlen] = '\0';
+  searchtriptr[cipherlen] = '\0';
 
-  // Trigraph
-  size_t segmentlen = 3;
+  // Find the occurences for each trigraph
+    
+    // Tri is 3!
+  size_t tri = 3;
 
-  size_t occurencelen = cipherlen/segmentlen;
+    // How many trigraphs in the ciphertext
+  size_t trigraphcount = cipherlen/tri;
 
-  size_t occurences[occurencelen];
-  size_t distances[occurencelen];
-  char *segments[occurencelen];
+    // Number of occurences of the trigraph
+  size_t occurences[trigraphcount];
+
+    // The trigraphs themselves.
+  char *trigraphs[trigraphcount];
   
   int i;
   int j;
-  for(i = 1; i <= occurencelen; i++)
+    
+    // Outer loop, for each trigraph in the ciphertext.
+  for(i = 1; i <= trigraphcount; i++)
     {
 
       occurences[i-1] = 1;
-      distances[i-1] = 0;
-      segments[i-1] = (char *) malloc(sizeof(char) * segmentlen);
-      searchptr = segmentptr + (segmentlen);
 
-      size_t searchlen = strlen(searchptr);
-      size_t searchoccurencelen = searchlen/segmentlen;
+      trigraphs[i-1] = (char *) malloc(sizeof(char) * tri);
+      searchtriptr = trigraphptr + (tri);
 
-      char *segmentcheck = (char *) malloc(sizeof(char) * segmentlen);
-      char *searchcheck= (char*) malloc(sizeof(char) * segmentlen);
-      strncpy(segmentcheck, segmentptr,segmentlen);
+        // length of the remaining cipher text being searched.
+      size_t searchlen = strlen(searchtriptr);
+        // Count of trigraphs in the remaining cipher text.
+      size_t searchtricount = searchlen/tri;
+
+        // Current trigraph that is being searched for.
+      char *currenttrigraph = (char *) malloc(sizeof(char) * tri);
+        // Current trigraph in search.
+      char *currenttrisearch= (char*) malloc(sizeof(char) * tri);
+        
+      strncpy(currenttrigraph, trigraphptr, tri);
       
-      for(j = 1; j <= searchoccurencelen; j++)
+        // Inner loop, for each following trigraph.
+      for(j = 1; j <= searchtricount; j++)
 	{
-	  strncpy(searchcheck, searchptr, segmentlen);
-	  // fprintf(stdout, "Comparing %s with %s\n", segmentcheck, searchcheck);
-	  if(strncmp(segmentcheck, searchcheck, segmentlen) == 0)
+	  strncpy(currenttrisearch, searchtriptr, tri);
+	  // fprintf(stdout, "Comparing %s with %s\n", currenttrigraph, currenttrisearch);
+	  if(strncmp(currenttrigraph, currenttrisearch, tri) == 0)
 	    {
 
 	      occurences[i-1]++;
-	      distances[i-1] = (searchptr - segmentptr);
-	      strncpy(segments[i-1], segmentptr, segmentlen);
-	      fprintf(stdout, "\nMatch FOUND! Segment length of %zu; Segment # %d, %s occurred for the %zu time and has a distance of %zu!\n", segmentlen, i, segments[i-1], occurences[i-1], distances[i-1]);
+	      strncpy(trigraphs[i-1], trigraphptr, tri);
+	      //   fprintf(stdout, "\nMatch FOUND! Segment length of %zu; Segment # %d, %s occurred for the %zu time and has a distance of %zu!\n", tri, i, trigraphs[i-1], occurences[i-1], distances[i-1]);
 	    }
-	   searchptr = searchptr + segmentlen;
+        
+        // Point to the next 3 characters
+	   searchtriptr = searchtriptr + tri;
 	}
-      segmentptr = segmentptr + segmentlen;
+      trigraphptr = trigraphptr + tri;
     }
 
-  for(i = 1; i <= (occurencelen); i++)
+  // Distances between the trigraph occurences.
+  int highest = 0;
+
+  for(i = 0; i < trigraphcount; i++)
     {
-      if(distances[i-1] != 0)
+      if(occurences[i] > highest)
 	{
-	  fprintf(stdout, "%zu ", distances[i-1]);
+	  highest = i;
 	}
     }
-  fprintf(stdout, "\n");
+
+  fprintf(stdout, "Highest is %zu, and the segment is %s\n", occurences[highest], trigraphs[highest]);
   
+
+  // loop through ciphertext adding distances between occurences
+  char *previousloc = 0;
+  char *currentloc = 0;
+  size_t distances[occurences[highest]];
+
+  // Initialize distances.
+  for(i = 0; i < occurences[highest]; i++)
+    {
+      distances[i] = 0;
+    }
+  
+  strncpy(trigraphptr, ciphertext, cipherlen);
+  j = 0;
+  for(i = 0; i < trigraphcount; i++)
+    {
+      
+      if(strncmp(trigraphptr, trigraphs[highest], tri) == 0)
+	{
+	  if(previousloc != 0) 
+	    {
+	      currentloc = trigraphptr;
+  	      distances[j] = (currentloc - previousloc);
+	      fprintf(stdout, "MatchFound with a distance of %zu\n", distances[j]);
+	      j++;
+	    }
+	  previousloc = trigraphptr;
+	}
+      trigraphptr = trigraphptr + tri;
+    }
   //  dictcheck("a", 1);
 }
